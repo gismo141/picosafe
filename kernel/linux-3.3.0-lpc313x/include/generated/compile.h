cee6a499134e2fbc83532867a7c5d88d79003649
gismo141 <michael.r141@gmail.com>
2014-07-02 14:03:49 +0200

    Fixed picosafes rootfs

--- kernel/linux-3.3.0-lpc313x/include/generated/compile.h
+++ kernel/linux-3.3.0-lpc313x/include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 49 */
+/* This file is auto generated, version 51 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#49 Wed Jul 2 10:29:34 CEST 2014"
+#define UTS_VERSION "#51 Wed Jul 2 13:57:35 CEST 2014"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "DebianVM"
 #define LINUX_COMPILER "gcc version 4.6.4 20120303 (prerelease) (GCC) "
