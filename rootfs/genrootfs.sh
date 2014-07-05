248e104ae2b73468ee88b21faa078c8f4431c6b1
gismo141 <michael.r141@gmail.com>
2014-07-05 14:09:49 +0200

    Divided 'genrootfs.sh'-script by functionality 'gensdcard.sh' for preparing the SD-Card (partitioning, copying bootloader and creating public partition) 'genrootfs.sh' for creating the root-filesystem, preparation for debootstrap-process

--- rootfs/genrootfs.sh
+++ rootfs/genrootfs.sh
@@ -2,10 +2,7 @@
 
 KERNELSRC="../kernel/linux-3.3.0-lpc313x/"  # path to kernel sources
 SDCARD="$1"
-KEYFILE="$2"
-# 2014-06-30: added bootloader argument, by Michael Riedel
-BOOTLOADER="$3"
-PEMFILE="$4"
+PEMFILE="$2"
 PASSWORD="picosafe"
 SWAPFILESIZE=64 # in MB
 # random string; will be used as name for cryptsetup. In general any string
@@ -15,10 +12,8 @@ ROOTFS="`tr -dc "[:alpha:]" < /dev/urandom | head -c 8`"
 
 # 2014-06-30: added usage-line for bootloader-argument, by Michael Riedel
 usage() {
-    echo "Usage: $0 DEVICE KEYFILE [PEMFILE]"
+    echo "Usage: $0 DEVICE [PEMFILE]"
     echo "    DEVICE:     path to device file of SD-card (e.g. /dev/sdb)"
-    echo "    KEYFILE:    key for decrypting kernel and initramfs (must match picosafe!)"
-    echo "    BOOTLOADER: bootloader to use (must match picosafe!)"
     echo "    PEMFILE:    pemfile for hiawathi webserver on initramfs"
 }
 
@@ -28,21 +23,7 @@ cd "`dirname $0`"
 
 # check if argument omitted
 if [ "$SDCARD" == "" ]; then
-  echo "Argument for device missing: $0 DEVICE KEYFILE BOOTLOADER" >&2
-  echo
-  usage
-  exit 1;
-fi
-
-if [ "$KEYFILE" == "" ]; then
-  echo "Argument for keyfile missing: $0 DEVICE KEYFILE BOOTLOADER" >&2
-  echo
-  usage
-  exit 1;
-fi
-# added check if bootloader argument was omitted, by Michael Riedel
-if [ "$BOOTLOADER" == "" ]; then
-  echo "Argument for bootloader missing: $0 DEVICE KEYFILE BOOTLOADER" >&2
+  echo "Argument for device missing: $0 DEVICE [PEMFILE]" >&2
   echo
   usage
   exit 1;
@@ -66,76 +47,11 @@ else
   fi
 fi
 
-
 # umount partitions
 umount "$SDCARD" 2>/dev/null
 for dev in "$SDCARD"*; do
   umount $dev 2>/dev/null
 done
-
-echo "Filling SD-card with random data..."
-#dd if=/dev/urandom of=$SDCARD
-
-SDCARDSIZE=`cat /sys/class/block/$BASENAME/size`
-#TEST="expr / 2000 / 2 + 500"
-LUKSPARTITIONSIZE=`expr "$SDCARDSIZE"  / 2000 / 2 + 500`
-echo -n "Luks partition size: "
-echo "$LUKSPARTITIONSIZE MB"
-
-# create partitions
-echo "Creating partion layout..."
-parted "$SDCARD" -a optimal --script -- mklabel msdos                # create empty partition table
-parted "$SDCARD" -a optimal --script -- mkpart primary ext3 2M 50M   # create ext3 partition (49MB)
-parted "$SDCARD" -a optimal --script -- mkpart primary ext3 1M 2M    # create boot partition (1MB)
-parted "$SDCARD" -a optimal --script -- mkpart primary ext3 50M "$LUKSPARTITIONSIZE"  # create LUKS partition (8000MB)
-parted "$SDCARD" -a optimal --script -- mkpart primary ext3 "$LUKSPARTITIONSIZE" "-1" # create data partition (remaining)
-
-# set partition 2 to bootit
-sfdisk -q --change-id "$SDCARD" 2 df
-
-# inform the OS of partition table changes
-partprobe "$SDCARD"
-
-echo "Sync..."
-sync
-
-sleep 10
-
-echo "Creating filesystems..."
-echo -e "\text3 on ${SDCARD}1"
-mkfs.ext3 -q -L "Picosafe" "$SDCARD"1
-echo -e "\tntfs on ${SDCARD}4"
-mkntfs -q -f -L "Picosafe public share" "$SDCARD"4
-
-sleep 10
-
-echo "Creating temporary mount point..."
-MNTPNT="`mktemp -d`"
-
-echo "Mounting Picosafe public share"
-mount "$SDCARD"4 "$MNTPNT"
-# 2014-06-30: corrected path for standard-files, by Michael Riedel
-echo "Copying files to Picosafe public share..."
-cp -r ../initramfs/welcome/* "$MNTPNT"
-cp ../user_manual/user_manual.pdf "$MNTPNT"
-
-echo "Sync..."
-sync
-
-echo "Umounting Picosafe public share..."
-umount -l "$MNTPNT"
-
-echo "Formating LUKS container..."
-echo "$PASSWORD" | cryptsetup -q luksFormat "${SDCARD}3"
-if [ $? != 0 ]; then
-  echo "Can't format container." >&2
-  exit 1;
-fi
-
-echo "Sync..."
-sync
-
-sleep 10
 
 echo "Opening LUKS container..."
 echo "$PASSWORD" | cryptsetup -q luksOpen "${SDCARD}3" "$ROOTFS"
@@ -144,19 +60,12 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-echo "Creating ext3 filesystem inside LUKS container..."
-mkfs.ext3 -q -b 1024 "/dev/mapper/$ROOTFS"
-
 echo "Mounting ext3 filesystem to $MNTPNT..."
 mount "/dev/mapper/$ROOTFS" "$MNTPNT"
 wget https://www.dropbox.com/s/9olqt0t7eo7mzi9/picosafe_rootfs.tar.gz
-# 2014-06-30: using extracted picosafe_rootfs instead of archive, by Michael Riedel
-# 2014-07-05: using uploaded picosafe_rootfs-archive, by Michael Riedel
 echo "Extracting linux filesystem to $MNTPNT..."
 tar xzf picosafe_rootfs.tar.gz -C "$MNTPNT"
 rm -rf picosafe_rootfs.tar.gz
-#echo "Copying linux filesystem to $MNTPNT..."
-#cp -a picosafe_rootfs/. "$MNTPNT"
 
 echo "Installing kernel modules..."
 export INSTALL_MOD_PATH="$MNTPNT"
@@ -207,34 +116,6 @@ umount "$MNTPNT"
 
 echo "Closing luks container..."
 cryptsetup luksClose "$ROOTFS"
-
-echo "Copying kernel..."
-mount "${SDCARD}1"  "$MNTPNT"
-../kernel/build.sh -k "$KEYFILE" -s "$PEMFILE" -o "$MNTPNT/zImage.crypt"
-
-echo "Copying config..."
-cp config "$MNTPNT/.config"
-# using argument bootloader or creating a new one, by Michael Riedel
-if [ "$BOOTLOADER" == "" ]; then
-  echo "Creating apex bootloader..."
-  APEXROM="`mktemp`"
-  chmod 600 "$BOOTLOADER"
-  ../bootloader/build.sh -a bootloader -k "$KEYFILE" -o "$BOOTLOADER"
-  RMAPEXROM=1
-fi
-
-echo "Copying apex bootloader..."
-dd if="$BOOTLOADER" of="${SDCARD}2" oflag=dsync 2>/dev/null
-
-if [ $RMAPEXROM ]; then
-  echo "Deleting temporary apex bootloader..."
-  rm "$BOOTLOADER"
-fi
-
-if [ $RMPEMFILE ]; then
-  echo "Deleting pemfile..."
-  rm $PEMFILE
-fi
 
 echo "Sync..."
 sync
